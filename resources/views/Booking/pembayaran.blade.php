caf4e1f351d776bc2645d86ea76f3462636bcdff
dickyaryag6 <dickyaryakesuma@gmail.com>
2018-12-02 01:34:50 +0700

    udah bisa upload file tapi belom bisa dimasukin ke db

--- resources/views/Booking/pembayaran.blade.php
+++ resources/views/Booking/pembayaran.blade.php
@@ -8,6 +8,13 @@
 <?php echo "Nomor rekening a.n. nama pemilik rek." ?>
 
 <?php echo "Sudah melakukan pembayaran? Upload bukti pembayaran anda" ?>
+<div class="card-body">
+  <form action="#" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+      <input type="file" id="bukti_pembayaran" name="bukti_pembayaran">
+    </div>
+  </form>
+</div>
 
 
 @endsection
